6e8980e01787e47de6bdb2d07be9990dc564444e
Marcos Amorim <markstm10@gmail.com>
2022-03-17 12:35:31 -0300

    feat: create route to get current quote data

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,45 @@
 import express from "express";
+import { json } from "stream/consumers";
 const cors = require("cors");
+
+import current from "./api/awesome-current";
 
 const app = express();
 app.use(cors());
+
+async function getCurrent(sourceCurrency: string, targetCurrency: string) {
+  let conc = sourceCurrency + targetCurrency;
+
+  try {
+    const { data } = await current.get(
+      `last/${sourceCurrency}-${targetCurrency}`
+    );
+
+    let formatedData = {
+      sourceCurrency: data[conc].code,
+      targetCurrency: data[conc].codein,
+      name: data[conc].name,
+      bid: data[conc].bid,
+      ask: data[conc].ask,
+      timestamp: data[conc].timestamp,
+    };
+
+    return formatedData;
+  } catch (error) {
+    console.log("erro ao buscar cotações: ", error);
+  }
+}
+
+app.get(`/current/:source-:target`, async (req, res) => {
+  const requestCurrent = await getCurrent(
+    `${req.params.source}`,
+    `${req.params.target}`
+  );
+
+  res.json({
+    ...requestCurrent,
+  });
+});
 
 app.get("/", (req, res) => {
   res.json({ msg: "all right" });
@@ -24,6 +61,8 @@ app.get("/contact", (req, res) => {
     ],
   });
 });
+
+// getCurrent("usd", "BRL");
 
 const PORT = process.env.PORT || 8877;
 
